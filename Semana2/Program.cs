eee0efa0ea49a575173da97cd7990d4738cb0f89
ander <anderalcaldeestrada@gmail.com>
2025-08-29 08:34:50 -0500

    Se fue agregado una nueva linea al codigo principal

--- Semana2/Program.cs
+++ Semana2/Program.cs
@@ -13,6 +13,7 @@ namespace Semana2
             Console.WriteLine("Hola Mundo");
             Console.WriteLine("Soy Jose");
             Console.WriteLine("Este es el primer repositorio en visual comunity");
+            Console.WriteLine("El NRC de la clase 6414");
         }
     }
 }
